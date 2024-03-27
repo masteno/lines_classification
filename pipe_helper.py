df934b316749e0a4d7da156593369be5898c0557
masten <masten.bourahma@yahoo.com>
2024-03-27 14:30:58 +0100

    second modificaition

--- pipe_helper.py
+++ pipe_helper.py
@@ -8,6 +8,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
+import random
 
 class read_data:
 
@@ -16,34 +17,30 @@ class read_data:
     self.files = list(self.root_dir + file for file in os.listdir(self.root_dir))
     #self.BATCH_SIZE = batch_size
   
-  def read_h5file(self, file_path, ids_flag):
+  def read_h5file(self, file_path):
     content = []
     with h5py.File(file_path, 'r') as f:
       for key in f.keys():
         dataset = f[key][()]
-        content.append(int(dataset[0]) if ids_flag else dataset)
+        content.append(dataset)
     return content
 
   def load_files_content (self):
     for file in self.files: 
-      if 'ids' in file:
-        ids = self.read_h5file(file, ids_flag=True)
+      if 'metas' in file:
+        metas = self.read_h5file(file)
       if 'vars' in file:
-        vars = self.read_h5file(file, ids_flag=False)
+        vars = self.read_h5file(file)
       if 'lines' in file:
-        lines = self.read_h5file(file, ids_flag=False)
+        lines = self.read_h5file(file)
       if 'labels' in file:
-        labels = self.read_h5file(file, ids_flag=False)
-    return ids, lines, labels, vars
+        labels = self.read_h5file(file)
+    return metas, lines, labels, vars
   
   def __add__ (self, B):
     i1, x1, y1, v1 = self.load_files_content ()
     i2, x2, y2, v2 = B.load_files_content()
-
-    ##  assert statement --> check that the two set have been extracted with the same 
-    # window size.
-
-    ##
+    assert (x1[0].shape[0] == x2[0].shape[0])
     return i1+i2, x1+x2, y1+y2, v1+v2
   
   def split_data (self, combine,B,max_norm, random_seed,TEST_SIZE,VAL_SIZE, BATCH_SIZE):
@@ -57,32 +54,34 @@ class read_data:
     '''
     # check for NANs
     if np.logical_and(combine, B is not None):
-      ids, lines, labels, vars = self + B
+      metas, lines, labels, vars = self + B
     else:
-      ids, lines, labels, vars = self.load_files_content()
+      metas, lines, labels, vars = self.load_files_content()
     ## checking for nans and full 0s ###################################
     idx = []
     for i,x in enumerate(lines):
       if np.logical_and (np.sum(x[:2]) != 0, np.any(np.isnan(x))==False):
         idx.append(i)
     ####################################################################
-    X, y, ids, V = np.array(lines)[idx], np.array(labels)[idx] , np.array(ids)[idx], np.array(vars)[idx]
-    
-    ### normalize X by max:
+    X, y, metas, V = np.array(lines)[idx], np.array(labels)[idx] , np.array(metas)[idx], np.array(vars)[idx]
     if max_norm:
       for i,x in enumerate(X):
         X[i] = X[i] / np.max(np.nan_to_num(np.abs(x),0))
     #######################
         
     X_train, X_test, y_train, y_test= train_test_split (X,y, test_size= TEST_SIZE, random_state= random_seed)
-    ids_train, ids_test = train_test_split (ids, test_size=TEST_SIZE, random_state=random_seed)
+    metas_train, metas_test = train_test_split (metas, test_size=TEST_SIZE, random_state=random_seed)
     vars_train, vars_test = train_test_split (V, test_size= TEST_SIZE, random_state= random_seed)
 
     if VAL_SIZE != 0:
       X_train, X_val, y_train, y_val = train_test_split (X_train,y_train, test_size= VAL_SIZE, random_state= random_seed)
-      ids_train, ids_val = train_test_split (ids_train, test_size=VAL_SIZE, random_state=random_seed)
+      metas_train, metas_val = train_test_split (metas_train, test_size=VAL_SIZE, random_state=random_seed)
+      vars_train, vars_val = train_test_split (vars_train, test_size=TEST_SIZE, random_state=random_seed)
       X_tval = torch.tensor(X_val).type(torch.float).unsqueeze(dim=1)
       t_tval = torch.tensor(y_val)#.type(torch.float).unsqueeze(dim=1)
+      V_tval = torch.tensor(vars_val).type(torch.float).unsqueeze(dim=1)
+      vars_val_data = torch.utils.data.TensorDataset(V_tval)
+      vars_val_dataloader =  DataLoader(vars_val_data, BATCH_SIZE, shuffle=False)
       val_data  = torch.utils.data.TensorDataset(X_tval, t_tval)
       val_dataloader  = DataLoader(val_data,  BATCH_SIZE, shuffle=False)
 
@@ -92,23 +91,37 @@ class read_data:
     X_ttest = torch.tensor(X_test).type(torch.float).unsqueeze(dim=1)
     t_ttest = torch.tensor(y_test)#.type(torch.float).unsqueeze(dim=1)
 
+    V_ttrain = torch.tensor(vars_train).type(torch.float).unsqueeze(dim=1)
+    V_ttest  = torch.tensor(vars_test).type(torch.float).unsqueeze(dim=1)
+
     train_data = torch.utils.data.TensorDataset(X_ttrain, t_ttrain)
     test_data  = torch.utils.data.TensorDataset(X_ttest, t_ttest)
+    vars_train_data = torch.utils.data.TensorDataset(V_ttrain)
+    vars_test_data = torch.utils.data.TensorDataset(V_ttest)
 
     torch.manual_seed(42)
-    train_dataloader = DataLoader(train_data, BATCH_SIZE, shuffle=True)
-    test_dataloader  = DataLoader(test_data,  BATCH_SIZE, shuffle=False)
+    train_dataloader = DataLoader(train_data, BATCH_SIZE, shuffle=False)
+    torch.manual_seed(42)
+    vars_train_dataloader = DataLoader(vars_train_data, BATCH_SIZE, shuffle=False)
 
+    test_dataloader  = DataLoader(test_data,  BATCH_SIZE, shuffle=False)
+    vars_test_dataloader = DataLoader(vars_test_data, BATCH_SIZE, shuffle=False)
+                                      
     #print(f"Dataloaders: {train_dataloader, test_dataloader}")
     print('****')
     print(f"Length of train dataloader: {len(train_dataloader)} batches of {BATCH_SIZE}")
     print(f"Length of test dataloader: {len(test_dataloader)} batches of {BATCH_SIZE}")
     if VAL_SIZE != 0: print(f"Length of val dataloader: {len(val_dataloader)} batches of {BATCH_SIZE}") 
-    ## add a branch to return validation set, ids and vars if needed 
+
+    metas_dataloaders = (metas_train, train_dataloader, metas_test, test_dataloader, metas_val, val_dataloader)
+    metas_dataloaders_noval = (metas_train, train_dataloader, metas_test, test_dataloader)
+
+    vars_ = (vars_train_dataloader, vars_test_dataloader, vars_val_dataloader)
+    vars_noval = (vars_train_dataloader, vars_test_dataloader)
     if VAL_SIZE != 0:
-      return ids_train, train_dataloader, ids_test, test_dataloader, ids_val, val_dataloader
+      return metas_dataloaders, vars_
     else:
-      return ids_train, train_dataloader,ids_test, test_dataloader
+      return metas_dataloaders_noval, vars_noval
 
 
 def plot_confusion_matrix (net,dataloader, save_path):
